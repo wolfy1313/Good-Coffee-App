7decb84b4205abb72641926871c3ebac66565431
jeremyHarrell <hillvulf@gmail.com>
2022-10-28 09:04:09 -0500

    updated home logo and made clickable

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='notHome'>
+      <Link to={'/coffee-shops'}><img src='https://github.com/wolfy1313/MERN-APP/blob/main/client/src/assets/coffee%20home%20logo.png?raw=true'/> </Link>
     </div>
   )
 }
